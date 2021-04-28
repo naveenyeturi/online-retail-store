8dfdc75aed1dd062fa24db42e1d780bc3ce370a6
naveen <rnaveenkumar016@gmail.com>
2021-04-28 15:33:14 +0530

    removed password from phpmailer for now

--- checkout.php
+++ checkout.php
@@ -211,8 +211,7 @@ error_reporting(-1);
 		$mail->isSMTP();                                            //Send using SMTP
 		$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		$mail->Username   = 'ffaakkee.praneeth@gmail.com';                     //SMTP username
-		$mail->Password   = 'praneethsquare';                               //SMTP password
+		                              //SMTP password
 		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
