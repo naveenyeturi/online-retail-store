84349ad0a52bf950e7a331c3b5cb0f0fde3187c5
naveenyeturi <38129036+naveenyeturi@users.noreply.github.com>
2021-06-06 14:39:13 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -225,13 +225,13 @@ error_reporting(-1);
 		$mail->isSMTP();                                            //Send using SMTP
 		$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		$mail->Username   = 'ffaakkee.praneeth@gmail.com';                     //SMTP username
-		$mail->Password   = 'praneethsquare';                               //SMTP password
+		$mail->Username   = 'ur_email@gmail.com';                     //SMTP username
+		$mail->Password   = 'ur_password';                               //SMTP password
 		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 		//Recipients
-		$mail->setFrom('ffaakkee.praneeth@gmail.com', 'MyMall');
+		$mail->setFrom('ur_email@gmail.com', 'MyMall');
 		$mail->addAddress($custemail, $custname);     //Add a recipient
 
 		$mail->isHTML(true);                                  //Set email format to HTML
